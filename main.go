a32b04ba0d2620562a168e506a4a7e8b22c4ab2f
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-12-01 18:53:46 -0500

    Add error handling for not passing enough arguments when running program

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	. "github.com/jdplumst/advent-of-code-2024/day01"
 )
 
 func main() {
+	if len(os.Args) <= 2 {
+		log.Fatal("Must run application with following format: go run . [day] [part]")
+	}
 	day := os.Args[1]
 	part := os.Args[2]
 	switch day {
