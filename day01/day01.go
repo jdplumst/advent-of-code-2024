bdd7831513aece668d58b79b2591aa8c5ccbd502
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-12-01 11:29:57 -0500

    Day 1 Part 2

--- day01/day01.go
+++ day01/day01.go
@@ -16,7 +16,8 @@ func Day01(part string) {
 		res := part1("./day01/input.txt")
 		fmt.Println("The result for day 1 part 1 is", res)
 	case "2":
-		part2("./day01/input.txt")
+		res := part2("./day01/input.txt")
+		fmt.Println("The result for day 1 part 1 is", res)
 	default:
 		panic("Must run either part 1 or part 2")
 	}
@@ -73,6 +74,55 @@ func part1(input string) int {
 	return res
 }
 
-func part2(input string) {
-	fmt.Println("part 2!")
+func part2(input string) int {
+	file, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	res := 0
+
+	list1, list2 := make([]int, 0), make([]int, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			continue
+		}
+
+		line := strings.Split(scanner.Text(), "   ")
+
+		a, err := strconv.Atoi(line[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		b, err := strconv.Atoi(line[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		list1 = append(list1, a)
+		list2 = append(list2, b)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	slices.Sort(list1)
+	slices.Sort(list2)
+
+	for _, elem1 := range list1 {
+		count := 0
+		for _, elem2 := range list2 {
+			if elem1 == elem2 {
+				count += 1
+			}
+		}
+		res += (elem1 * count)
+	}
+
+	return res
 }
