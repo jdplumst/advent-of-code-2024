29992880f9b8ab897e3d3d357f57b16b76c71f92
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-12-03 01:18:24 -0500

    Day 3 Part 2

--- day03/day03.go
+++ day03/day03.go
@@ -70,6 +70,75 @@ func part1(input string) int {
 	return res
 }
 
-func part2(input string) int { return 1 }
+func part2(input string) int {
+	file, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	res := 0
+	check := ""
+	do := true
+	checkDo := ""
+	a := 0
+	b := 0
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanBytes)
+	for scanner.Scan() {
+		if scanner.Text() == "m" {
+			check = "m"
+		} else if scanner.Text() == "u" && check == "m" {
+			check = "mu"
+		} else if scanner.Text() == "l" && check == "mu" {
+			check = "mul"
+		} else if scanner.Text() == "(" && check == "mul" {
+			check = "mul("
+		} else if digitCheck.MatchString(scanner.Text()) && check == "mul(" {
+			digit, _ := strconv.Atoi(scanner.Text())
+			a = a * 10
+			a += digit
+		} else if digitCheck.MatchString(scanner.Text()) && check == "mul(," && do {
+			digit, _ := strconv.Atoi(scanner.Text())
+			b = b * 10
+			b += digit
+		} else if scanner.Text() == "," && check == "mul(" {
+			check = "mul(,"
+		} else if scanner.Text() == ")" && check == "mul(," && do {
+			check = ""
+			res += a * b
+			a = 0
+			b = 0
+		} else if scanner.Text() == "d" && checkDo == "" {
+			checkDo = "d"
+		} else if scanner.Text() == "o" && checkDo == "d" {
+			checkDo = "do"
+		} else if scanner.Text() == "(" && checkDo == "do" {
+			checkDo = "do("
+		} else if scanner.Text() == ")" && checkDo == "do(" {
+			do = true
+			checkDo = ""
+		} else if scanner.Text() == "n" && checkDo == "do" {
+			checkDo = "don"
+		} else if scanner.Text() == "'" && checkDo == "don" {
+			checkDo = "don'"
+		} else if scanner.Text() == "t" && checkDo == "don'" {
+			checkDo = "don't"
+		} else if scanner.Text() == "(" && checkDo == "don't" {
+			checkDo = "don't("
+		} else if scanner.Text() == ")" && checkDo == "don't(" {
+			do = false
+			checkDo = ""
+		} else {
+			check = ""
+			checkDo = ""
+			a = 0
+			b = 0
+		}
+
+	}
+	return res
+}
 
 var digitCheck = regexp.MustCompile(`^[0-9]+$`)
