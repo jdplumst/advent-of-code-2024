80fd923c2cc2cecb9b7da9db5006edc9cfe2bd2d
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-12-05 13:06:50 -0500

    Day 5 Part 2

--- day05/day05.go
+++ day05/day05.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -15,7 +16,7 @@ func Day05(part string) {
 		res := part1("./day05/input.txt")
 		fmt.Println("The result for day 5 part 1 is", res)
 	case "2":
-		res := part2("./day04/input.txt")
+		res := part2("./day05/input.txt")
 		fmt.Println("The result for day 5 part 2 is", res)
 	default:
 		log.Fatal("Must run either part 1 or part 2")
@@ -80,7 +81,68 @@ func part1(input string) int {
 }
 
 func part2(input string) int {
-	return 1
+	file, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	res := 0
+
+	rulesCheck := true
+
+	rules := make([][]int, 0)
+	incorrectUpdates := make([][]int, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			rulesCheck = false
+			continue
+		}
+
+		if rulesCheck {
+			c := strings.Split(scanner.Text(), "|")
+			left, err := strconv.Atoi(c[0])
+			if err != nil {
+				log.Fatal(err)
+			}
+			right, err := strconv.Atoi(c[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			x := []int{left, right}
+			rules = append(rules, x)
+		} else {
+			c := strings.Split(scanner.Text(), ",")
+			x := make([]int, 0)
+			for _, e := range c {
+				n, err := strconv.Atoi(e)
+				if err != nil {
+					log.Fatal(err)
+				}
+				x = append(x, n)
+			}
+			correct := checkCorrect(x, rules)
+			if correct {
+				continue
+			}
+			for {
+				if correct {
+					break
+				}
+				x = fixLine(x, rules)
+				correct = checkCorrect(x, rules)
+			}
+			incorrectUpdates = append(incorrectUpdates, x)
+		}
+	}
+
+	for _, e := range incorrectUpdates {
+		res += e[len(e)/2]
+	}
+
+	return res
 }
 
 func checkCorrect(line []int, rules [][]int) bool {
@@ -96,4 +158,17 @@ func checkCorrect(line []int, rules [][]int) bool {
 	}
 
 	return correct
+}
+
+func fixLine(line []int, rules [][]int) []int {
+	for i := 0; i < len(line); i++ {
+		for j := i + 1; j < len(line); j++ {
+			for k, _ := range rules {
+				if rules[k][0] == line[j] && rules[k][1] == line[i] {
+					reflect.Swapper(line)(i, j)
+				}
+			}
+		}
+	}
+	return line
 }
