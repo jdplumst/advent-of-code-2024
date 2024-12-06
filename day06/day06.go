4358aa3d0ded0aa4bf3b4550a174a2198883fe8e
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2024-12-06 12:12:26 -0500

    Day 6 Part 2

--- day06/day06.go
+++ day06/day06.go
@@ -62,7 +62,32 @@ func part1(input string) int {
 }
 
 func part2(input string) int {
-	return 1
+	file, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	res := 0
+
+	guard_map := make([][]string, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		c := strings.Split(scanner.Text(), "")
+		guard_map = append(guard_map, c)
+	}
+
+	for i := 0; i < len(guard_map); i++ {
+		for j := 0; j < len(guard_map); j++ {
+			loop := checkLoop(guard_map, i, j)
+			if loop {
+				res++
+			}
+		}
+	}
+
+	return res
 }
 
 func findGuard(guard_map [][]string) (int, int, direction, error) {
@@ -152,9 +177,91 @@ func countPositions(guard_map [][]string) int {
 	return count
 }
 
+func checkLoop(guard_map [][]string, x, y int) bool {
+	if guard_map[x][y] != "." {
+		return false
+	}
+
+	gx, gy, d, err := findGuard(guard_map)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	obstructed_map := make([][]string, len(guard_map))
+	for i := range guard_map {
+		obstructed_map[i] = make([]string, len(guard_map[i]))
+		copy(obstructed_map[i], guard_map[i])
+	}
+
+	obstructed_map[x][y] = "#"
+
+	for {
+		switch d {
+		case direction(up):
+			gx--
+			if gx < 0 {
+				return false
+			}
+			if strings.Contains(obstructed_map[gx][gy], "U") {
+				return true
+			} else {
+				obstructed_map[gx][gy] += "U"
+				if strings.Contains(obstructed_map[gx][gy], "#") {
+					d = direction(right)
+					gx++
+				}
+			}
+		case direction(right):
+			gy++
+			if gy >= len(obstructed_map[gx]) {
+				return false
+			}
+			if strings.Contains(obstructed_map[gx][gy], "R") {
+				return true
+			} else {
+				obstructed_map[gx][gy] += "R"
+				if strings.Contains(obstructed_map[gx][gy], "#") {
+					d = direction(down)
+					gy--
+				}
+			}
+		case direction(down):
+			gx++
+			if gx >= len(obstructed_map) {
+				return false
+			}
+			if strings.Contains(obstructed_map[gx][gy], "D") {
+				return true
+			} else {
+				obstructed_map[gx][gy] += "D"
+				if strings.Contains(obstructed_map[gx][gy], "#") {
+					d = direction(left)
+					gx--
+				}
+			}
+		case direction(left):
+			gy--
+			if gy < 0 {
+				return false
+			}
+			if strings.Contains(obstructed_map[gx][gy], "L") {
+				return true
+			} else {
+				obstructed_map[gx][gy] += "L"
+				if strings.Contains(obstructed_map[gx][gy], "#") {
+					d = direction(up)
+					gy++
+				}
+			}
+		}
+	}
+}
+
 func printGuardMap(guard_map [][]string) {
 	for i := 0; i < len(guard_map); i++ {
 		fmt.Println(guard_map[i])
 	}
 	fmt.Println("-------------------------------")
 }
+
+// 1111 is too low
